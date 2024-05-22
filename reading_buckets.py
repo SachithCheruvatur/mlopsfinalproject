a3fe456a70f7322883240dccca52d1b7016e6b27
Sachith Cheruvatur <139083406+SachithCheruvatur@users.noreply.github.com>
2024-05-22 05:37:48 +0530

    Update reading_buckets.py

--- reading_buckets.py
+++ reading_buckets.py
@@ -11,6 +11,8 @@ if response.status_code == 200:
   # Get the content as a string
   content = response.text
   # Print the content
+  print("content",type(content))
+  print("response", type(response))
   print(content)
 else:
   print(f"Error: Could not retrieve file. Status code: {response.status_code}")

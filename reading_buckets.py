f1010a3ebace264fde7983e8569207935f4767bc
Sachith Cheruvatur <139083406+SachithCheruvatur@users.noreply.github.com>
2024-05-22 15:31:31 +0530

    Update reading_buckets.py

--- reading_buckets.py
+++ reading_buckets.py
@@ -1,7 +1,7 @@
 import requests
 
 # Define the URL of the test file
-url = "https://storage.googleapis.com/mlopsfileprojectbucket/testfile.json"
+url = "https://storage.googleapis.com/mlopsfileprojectbucket/Redacted_Catalog.json"
 
 # Send a GET request to retrieve the file content
 response = requests.get(url)
@@ -11,8 +11,6 @@ if response.status_code == 200:
   # Get the content as a string
   content = response.text
   # Print the content
-  print("content",type(content))
-  print("response", type(response))
-  print(content)
+  print(content[1])
 else:
   print(f"Error: Could not retrieve file. Status code: {response.status_code}")
